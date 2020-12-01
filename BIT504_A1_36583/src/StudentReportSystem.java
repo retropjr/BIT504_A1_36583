42d5dd980594189b637cea430a184ab86c0e6e83
Richard Porter <retropjr@hotmail.com>
2020-12-01 17:05:46 +1300

    Added ConsoleIO class to deal with input/output.

--- BIT504_A1_36583/src/StudentReportSystem.java
+++ BIT504_A1_36583/src/StudentReportSystem.java
@@ -16,7 +16,7 @@ public class StudentReportSystem {
 			boolean exitProgram = false;
 			while (!exitProgram) {
 				//display menu
-				displayMenu();
+				consoleIO.displayMenu();
 				
 				//get user option
 				int option = getUserOption();
@@ -98,16 +98,7 @@ public class StudentReportSystem {
 	}
 	
 	
-	public static void displayMenu() {
-		System.out.println("Student Report System");
-		System.out.println("---------------------");
-		System.out.println();
-		System.out.println("1) Display student marks.");
-		System.out.println("2) Display student grades.");
-		System.out.println("3) Add new student.");
-		System.out.println("4) Remove student.");
-		System.out.println("5) Exit.");
-	}
+	
 	
 	
 	public static int getUserOption() {
---
+++ BIT504_A1_36583/src/consoleIO.java
@@ -0,0 +1,14 @@
+
+public class consoleIO {
+	
+	public static void displayMenu() {
+		System.out.println("Student Report System");
+		System.out.println("---------------------");
+		System.out.println();
+		System.out.println("1) Display student marks.");
+		System.out.println("2) Display student grades.");
+		System.out.println("3) Add new student.");
+		System.out.println("4) Remove student.");
+		System.out.println("5) Exit.");
+	}
+}
